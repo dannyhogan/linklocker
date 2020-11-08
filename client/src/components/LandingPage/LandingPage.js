f82d548b8a880816dfd83584b2087c396f9af0d4
Dan Hogan <hogan.daniel@hotmail.com>
2020-11-07 22:02:08 -0800

    import particles

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -3,26 +3,27 @@ import { Link } from 'react-router-dom';
 import './LandingPage.scss'
 import { Button } from '@material-ui/core';
 import LandingPageForm from '../LandingPageForm/LandingPageForm';
+import Particles from 'react-particles-js';
 
 const LandingPage = () => {
 
   return (
 
     <section className="LandingPage">
-      <h1>One link for everything, stored securely.</h1>
+      <h1>One page for all your links, stored securely.</h1>
 
       <LandingPageForm />
 
       <div className="auth-container">
 
-        <div>
+        <div className="auth-redirect-container">
           <h4>Need an account?</h4>
           <Link to="/signup">
             <Button variant="outlined">Sign Up</Button>
           </Link>
         </div>
 
-        <div>
+        <div className="auth-redirect-container">
           <h4>Existing user?</h4>
           <Link to="/login">
             <Button variant="outlined">Login</Button>
@@ -30,7 +31,26 @@ const LandingPage = () => {
         </div>
 
       </div>
-
+      <Particles
+        className="particles"
+        params={{
+          "particles": {
+            "number": {
+              "value": 50
+            },
+            "size": {
+              "value": 3
+            }
+          },
+          "interactivity": {
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              }
+            }
+          }
+        }} />
     </section>
   );
 }
