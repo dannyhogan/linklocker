512efcb7a6ceed1dcac1e0ca58be1ef118787c88
Dan Hogan <hogan.daniel@hotmail.com>
2020-11-09 20:07:44 -0800

    header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
-import { useLocation, useParams } from 'react-router-dom';
 import './Header.scss'
 
 const Header = () => {
-  const params = useParams();
-  const location = useLocation();
-
   return (
     <div className="Header">
       <Logo />
